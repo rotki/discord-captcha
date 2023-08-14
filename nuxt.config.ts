89b67188516fce1ecd98b3e5358cfccbd147801c
Konstantinos Paparas <kelsos86@gmail.com>
2023-08-14 11:21:48 +0200

    feat: adds favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,50 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'discord.rotki.com',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'msapplication-TileColor', content: '#00aba9' },
+        { name: 'theme-color', content: '#ffffff' },
+      ],
+      link: [
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.png',
+          sizes: '180x180',
+        },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-32x32.png',
+          sizes: '32x32',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-16x16.png',
+          sizes: '16x16',
+        },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest',
+          crossorigin: 'use-credentials',
+        },
+        {
+          rel: 'mask-icon',
+          href: '/safari-pinned-tab.svg',
+          color: '#5bbad5',
+        },
+      ],
+    },
+  },
   ssr: true,
   modules: [
     '@nuxt/devtools',
