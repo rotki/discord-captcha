b914744d6076ff3914e1988b4ba66d6b90637ecc
Konstantinos Paparas <kelsos86@gmail.com>
2024-01-26 20:49:48 +0100

    chore: update dependencies

--- modules/ui-library/runtime/plugin.ts
+++ modules/ui-library/runtime/plugin.ts
@@ -8,24 +8,26 @@ import {
   RiTeamLine,
   RiTwitterLine,
   RiWechatLine,
-  RuiPlugin,
+  createRui,
 } from '@rotki/ui-library';
-import { defineNuxtPlugin } from '#app';
 import '@fontsource/roboto/latin.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(RuiPlugin, {
-    mode: 'light',
-    icons: [
-      RiGithubLine,
-      RiMailSendLine,
-      RiTwitterLine,
-      RiTeamLine,
-      RiBug2Line,
-      RiQuestionnaireLine,
-      RiBook2Line,
-      RiNewspaperLine,
-      RiWechatLine,
-    ],
+  const rui = createRui({
+    theme: {
+      mode: 'light',
+      icons: [
+        RiGithubLine,
+        RiMailSendLine,
+        RiTwitterLine,
+        RiTeamLine,
+        RiBug2Line,
+        RiQuestionnaireLine,
+        RiBook2Line,
+        RiNewspaperLine,
+        RiWechatLine,
+      ],
+    },
   });
+  nuxtApp.vueApp.use(rui);
 });
